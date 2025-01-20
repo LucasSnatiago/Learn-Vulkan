9f3abb47259bf8479137cc48627deb231aa3bdfe
Lucas Santiago <lu.santi.oli@gmail.com>
2025-01-20 17:52:45 -0300

    feat: create and destroy device

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -7,6 +7,10 @@
 #include <vk_types.h>
 #include <vk_initializers.h>
 
+#include <VkBootstrap.h>
+
+constexpr bool bUseValidationLayers = false;
+
 void VulkanEngine::init()
 {
 	// We initialize SDL and create a window with it.
@@ -21,16 +25,31 @@ void VulkanEngine::init()
 		window_flags
 	);
 
+	// Initting everything related to vulkan
+	init_vulkan();
+	init_swapchain();
+	init_commands();
+	init_sync_structures();
+
 	//everything went fine
 	_isInitialized = true;
 }
+
 void VulkanEngine::cleanup()
 {
-	if (_isInitialized) {
+	if (!_isInitialized)
+		return;
 
-		SDL_DestroyWindow(_window);
-	}
+	destroy_swapchain();
+
+	vkDestroySurfaceKHR(_instance, _surface, nullptr);
+	vkDestroyDevice(_device, nullptr);
+
+	vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+	vkDestroyInstance(_instance, nullptr);
+	SDL_DestroyWindow(_window);
 }
+
 
 void VulkanEngine::draw()
 {
@@ -54,4 +73,95 @@ void VulkanEngine::run()
 
 		draw();
 	}
+}
+
+void VulkanEngine::init_vulkan()
+{
+    vkb::InstanceBuilder builder;
+
+	// Make the vulkan instance with the default debug options
+	auto inst_ret = builder.set_app_name("Example vulkan application")
+		.request_validation_layers(bUseValidationLayers)
+		.use_default_debug_messenger()
+		.require_api_version(1, 3, 0)
+		.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	_instance = vkb_inst.instance;
+	_debug_messenger = vkb_inst.debug_messenger;
+
+	SDL_Vulkan_CreateSurface(_window, _instance, nullptr, &_surface);
+
+	// Vulkan 1.3 features
+	VkPhysicalDeviceVulkan13Features features13 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+	features13.dynamicRendering = true;
+	features13.synchronization2 = true;
+
+	// Vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12 { .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+	vkb::PhysicalDeviceSelector selector { vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 3)
+		.set_required_features_13(features13)
+		.set_required_features_12(features12)
+		.set_surface(_surface)
+		.select()
+		.value();
+
+	// Creating the final vulkan device
+	vkb::DeviceBuilder deviceBuilder { physicalDevice };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the vkDevice handle used in the rest of a vulkan application
+	_device = vkbDevice.device;
+	_chosenGPU = physicalDevice.physical_device;
+}
+void VulkanEngine::init_swapchain()
+{
+    create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+void VulkanEngine::init_commands()
+{
+    //nothing yet
+}
+void VulkanEngine::init_sync_structures()
+{
+    //nothing yet
+}
+
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+	vkb::SwapchainBuilder swapchainBuiler { _chosenGPU, _device, _surface };
+
+	_swapchainImageFormat = VK_FORMAT_B8G8R8_UNORM;
+
+	vkb::Swapchain vkbSwapchain = swapchainBuiler
+		// .use_default_format_selection()
+		.set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+		// Use vsync preset mode
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+		.set_desired_extent(width, height)
+		.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+		.build()
+		.value();
+
+	_swapchainExtent = vkbSwapchain.extent;
+
+	// Store swapchain and its related images
+	_swapchain = vkbSwapchain.swapchain;
+	_swapchainImages = vkbSwapchain.get_images().value();
+	_swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::destroy_swapchain()
+{
+	vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+	// Destroy swapchain resources
+	for (auto &image : _swapchainImageViews)
+		vkDestroyImageView(_device, image, nullptr);
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -5,9 +5,11 @@
 
 #include <vk_types.h>
 
+#include <vector>
 class VulkanEngine {
 public:
 
+	// Windowing system
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 
@@ -26,4 +28,27 @@ public:
 
 	//run main loop
 	void run();
+
+	// Vulkan engine
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t heigh);
+	void destroy_swapchain();
 };
